7e7ff6a1767e6c9ba0c3040f70dca98701aca016
PoroPoro <virtuax@protonmail.com>
2019-02-26 11:59:02 +0000

    Flat Board Off-centre.
    Off-centre board.

--- isometricgame/core/src/main/java/com/isometricgame/core/Play.java
+++ isometricgame/core/src/main/java/com/isometricgame/core/Play.java
@@ -7,18 +7,20 @@ import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.IsometricTiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class Play implements Screen {
 
     private TiledMap map; 
-	private OrthogonalTiledMapRenderer renderer; 
+	private IsometricTiledMapRenderer renderer; 
 	private OrthographicCamera camera; 
 
     @Override
 	public void render (float delta) {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
+		renderer.setView(camera); 
 	 	renderer.render();
 	}
 
@@ -32,7 +34,7 @@ public class Play implements Screen {
     @Override
     public void show() {   
 		map = new TmxMapLoader().load("/home/dev1/GitProjects/IsometricGame/isometricgame/core/src/main/java/com/isometricgame/core/map.tmx"); 
-		renderer = new OrthogonalTiledMapRenderer(map); 
+		renderer = new IsometricTiledMapRenderer(map); 
 		camera = new OrthographicCamera(); 
     }
 
