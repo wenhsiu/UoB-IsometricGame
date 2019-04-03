5a71aa0fefc0ceec5e4aeef61113200d80602e7b
PoroPoro <virtuax@protonmail.com>
2019-04-03 16:58:42 +0100

    Updated background colour
    Pretty blue.

--- isometricgame/core/src/main/java/com/isometricgame/core/GameMAIN.java
+++ isometricgame/core/src/main/java/com/isometricgame/core/GameMAIN.java
@@ -63,7 +63,7 @@ public class GameMAIN extends GameState {
     	
     	
     	
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(0x64/255.0f, 0x95/255.0f, 0xed/255.0f,0xff/255.0f);
 		Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
 	 	
 	 	//collect coin
