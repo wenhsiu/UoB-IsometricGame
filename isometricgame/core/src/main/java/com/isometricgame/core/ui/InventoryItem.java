a7afff0e0d4c73a830c4a526390f1aaaaf4af6c1
Lizzie Hull <eh14388@blueyonder.co.uk>
2019-05-18 15:01:39 +0100

    inventory refactoring part one

--- isometricgame/core/src/main/java/com/isometricgame/core/ui/InventoryItem.java
+++ isometricgame/core/src/main/java/com/isometricgame/core/ui/InventoryItem.java
@@ -1,33 +1,16 @@
 package com.isometricgame.core.ui;
 
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 public class InventoryItem extends Image {
 
-    // TAKE OUT WHAT ISN'T BEING USED AND/OR REFACTOR
+    // TODO: BADDIES TAKE COINS? COOL BUT THEN YOU CANNOT PICK ANY MORE UP ATM
 
     public enum ItemTypeID {
         COIN, MEDAL, NONE;
     }
 
-    private int itemAttributes;
-    private int itemUseType;
-    private int itemUseTypeValue;
     private ItemTypeID itemTypeID;
-    private String itemShortDescription;
-    private int itemValue;
-
-    public InventoryItem(TextureRegion textureRegion, int itemAttributes, 
-                        ItemTypeID itemTypeID, int itemUseType) {
-        super(textureRegion);
-        
-        this.itemTypeID = itemTypeID;
-        this.itemAttributes = itemAttributes;
-        this.itemUseType = itemUseType;
-
-    }
 
     public InventoryItem() {
         super();
@@ -36,28 +19,6 @@ public class InventoryItem extends Image {
     public InventoryItem(InventoryItem inventoryItem) {
         super();
         this.itemTypeID = inventoryItem.getItemTypeID();
-        this.itemAttributes = inventoryItem.getItemAttributes();
-        this.itemUseType = inventoryItem.getItemUseType();
-        this.itemUseTypeValue = inventoryItem.getItemUseTypeValue();
-        this.itemShortDescription = inventoryItem.getItemShortDescription();
-        this.itemValue = inventoryItem.getItemValue();
-
-    }
-
-    public int getItemUseTypeValue() {
-        return itemUseTypeValue;
-    }
-
-    public void setItemUseTypeValue(int itemUseTypeValue) {
-        this.itemUseTypeValue = itemUseTypeValue;
-    }
-
-    public int getItemValue() {
-        return itemValue;
-    }
-
-    public void setItemValue(int itemValue) {
-        this.itemValue = itemValue;
     }
 
     public ItemTypeID getItemTypeID() {
@@ -68,47 +29,7 @@ public class InventoryItem extends Image {
         this.itemTypeID = itemTypeID;
     }
 
-    public int getItemAttributes() {
-        return itemAttributes;
-    }
-
-    public void setItemAttributes(int itemAttributes) {
-        this.itemAttributes = itemAttributes;
-    }
-
-    public int getItemUseType() {
-        return itemUseType;
-    }
-
-    public void setItemUseType(int itemUseType) {
-        this.itemUseType = itemUseType;
-    }
-
-    public String getItemShortDescription() {
-        return itemShortDescription;
-    }
-
-    public void setItemShortDescription(String itemShortDescription) {
-        this.itemShortDescription = itemShortDescription;
-    }
-
     public boolean isSameItemType(InventoryItem possibleInventoryItem) {
         return itemTypeID == possibleInventoryItem.getItemTypeID();
     }
-
-    public int getTradeValue() {
-        // can sell items for half their original price
-        if(itemValue >= 0) {
-            return MathUtils.floor(itemValue * .5f);
-        }
-        else {
-            return 0;
-        }
-    }
-
-    
-
-
-
-
 }
